fc22d3fab7b71fbba75e67d4fd9c2d34feb1ea00
gwkim <gwkim@hudaters.com>
2026-06-10 05:29:13 +0000

    recommit

--- install.sh
+++ install.sh
@@ -13,7 +13,9 @@ CAM_CONFIG_FILE="$PROJECT_DIR/cameras.json"
 SYS_CONFIG_FILE="$PROJECT_DIR/system_config.json"
 TARGET_SCRIPT="$PROJECT_DIR/multi_event.py"
 DX_DIR="$USER_HOME/dx-runtime"
+
 sudo apt install ssh -y
+
 echo "====================================================="
 echo " Raspberry Pi Edge AI CCTV 서비스 하이브리드 설치"
 echo "====================================================="
