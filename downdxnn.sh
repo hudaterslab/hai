4e2f55e8ae8ec88877d60025097432b2b0cee800
dhkim <dhkim@hudaters.com>
2026-08-10 16:40:05 +0900

    fix: download both HanJin main models

--- downdxnn.sh
+++ downdxnn.sh
@@ -2,4 +2,5 @@ wget -O yolov8m-face_ppu.dxnn https://huggingface.co/HudatersU/sample_3.2.0/reso
 wget -O helmet_260622.dxnn https://huggingface.co/HudatersU/sample_3.2.0/resolve/main/helmet_260622.dxnn
 wget -O yolov8m-face.dxnn https://huggingface.co/HudatersU/sample_3.2.0/resolve/main/yolov8m-face.dxnn
 wget -O license_plate_detector_v2.dxnn https://huggingface.co/HudatersU/sample_3.2.0/resolve/main/license_plate_detector_v2.dxnn
+wget -O hanjin_cctv_v2.dxnn https://huggingface.co/HudatersU/sample_3.2.0/resolve/main/hanjin_cctv_v2.dxnn
 wget -O hanjin_cctv_v3.dxnn https://huggingface.co/HudatersU/sample_3.2.0/resolve/main/hanjin_cctv_v3.dxnn
